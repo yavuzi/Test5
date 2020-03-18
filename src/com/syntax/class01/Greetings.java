ea7abd41fa64cd905f91f6580a77292d8e44423e
yavuzi <yavuzidug@gmail.com>
2020-03-18 18:40:07 -0500

    Adding Java Project code

---
+++ src/com/syntax/class01/Greetings.java
@@ -0,0 +1,16 @@
+package com.syntax.class01;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//println vs print
+		
+		System.out.println("Good Morning");
+		System.out.print("Good Afternoon");
+		System.out.println("Good Evening");
+		System.out.println("Good Night");
+
+	}
+
+}
---
+++ src/com/syntax/class01/JavaProgrammer.java
@@ -0,0 +1,14 @@
+package com.syntax.class01;
+
+public class JavaProgrammer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This is my second Java Program
+		System.out.println("I am a Java Programmer");
+		System.out.println("I learn Java to do automation later");
+		System.out.println("I am using Eclipse to write my code");
+	}
+	
+
+}
